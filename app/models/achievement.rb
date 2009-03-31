169c72b0bdacbb5d48c5ab3aa2f91692bfad077b
Tom Insam <tom@jerakeen.org>
2009-03-31 17:11:06 +0100

    meh, drop this, it's causing problems. Probably not needed anyway.

--- app/models/achievement.rb
+++ app/models/achievement.rb
@@ -1,7 +1,7 @@
 class Achievement < ActiveRecord::Base
     has_many :character_achievements
     validates_uniqueness_of :armory_id
-    validates_uniqueness_of :name
+    #validates_uniqueness_of :name
     
     def wowhead_url
         "http://www.wowhead.com/?achievement=#{ self.armory_id }"
