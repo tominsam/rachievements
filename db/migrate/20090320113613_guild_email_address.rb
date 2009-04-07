8989b2a91c98fa2f88e3042638475a39f11510b7
Tom Insam <tom@jerakeen.org>
2009-04-07 23:29:57 +0100

    just drop this for now.

--- db/migrate/20090320113613_guild_email_address.rb
+++ db/migrate/20090320113613_guild_email_address.rb
@@ -3,7 +3,7 @@ class GuildEmailAddress < ActiveRecord::Migration
       add_column :guilds, :email, :string, :limit => 100
       
       # testing
-      Guild.find_by_name("unassigned variable").update_attributes!( :email => "tom@jerakeen.org" )
+      #Guild.find_by_name("unassigned variable").update_attributes!( :email => "tom@jerakeen.org" )
   end
 
   def self.down
