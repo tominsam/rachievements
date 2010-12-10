5a072b4ae158e23fce23c09086af5d8a3ac7a5fb
Tom Insam <tom@jerakeen.org>
2010-12-10 20:36:42 +0100

    fix mailer

--- app/models/guild_mailer.rb
+++ app/models/guild_mailer.rb
@@ -1,17 +1,19 @@
 class GuildMailer < ActionMailer::Base
 
     helper :application
+
+    # shouldn't be here
+    default_url_options[:host] = "achievements.movieos.org"
     
     default :from => "Tom's Magical Mail Sending Robot <tom@movieos.org>"
 
-    
     def GuildMailer.send_weekly_summaries
         # call this method on mondays. It can cope with being called more than
         # once on a monday, but don't call it all the time.
         Guild.where( [ "email_sent_at < ? or email_sent_at is null", Time.now - 2.days ] ).each{|g|
             if !g.email.blank?
                 puts "#{ g.to_s }"
-                GuildMailer.deliver_weekly_summary( g )
+                GuildMailer.weekly_summary( g ).deliver
             end
         }
     end
@@ -28,16 +30,17 @@ class GuildMailer < ActionMailer::Base
         
         # update guild object to note last send date. Do this before sending, to avoid nasty failure mode.
         guild.update_attributes!( :email_sent_at => Time.now )
-
-        mail( :to => email, :subject => "The magical world of #{ guild.name }, week beginning #{ (Time.now - 7.days).strftime("%d %B") }" )
         
-        @guild = guild
-        @all_items = guild.character_achievements.all( :conditions => [ 'character_achievements.created_at >= ?', Date.today - 1.week ] )
-        @people = all_items.group_by{|i| i.character }.sort_by{|character, items| [ character.achpoints * -1, character.rank ] }
-        @level_80 = guild.characters.count(:conditions => { :level => 80 } )
-        @total = guild.characters.count
-        @levels = all_items.select{|i| i.achievement.name.match(/^Level \d+/) }.map{|i| [ i.character, i.achievement.name.downcase ] }.sort_by{|char, level| level }.reverse.uniq_by{|character, level| character }
+        subject = "The magical world of #{ guild.name }, week beginning #{ (Time.now - 7.days).strftime("%d %B") }"
 
+        @guild = guild
+        @items = @guild.guild_achievements.where( [ 'created_at >= ?', Date.today - 1.week ] ).includes(:character)
+        @people = @items.group_by{|i| i.character }.sort_by{|character, items| [ character.achpoints * -1, character.rank ] }
+        @level_85 = @guild.characters.count(:conditions => [ "level >= 85" ] )
+        @total = @guild.characters.count
+        @levels = @items.select{|i| i.achievement.name.match(/^Level \d+/) }.map{|i| [ i.character, i.achievement.name.downcase ] }.sort_by{|char, level| level }.reverse.uniq_by{|character, level| character }
+        
+        mail( :to => email, :subject => subject )
     end
 
 end
